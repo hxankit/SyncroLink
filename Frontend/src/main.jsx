39624b0ad16c39ab3f27e5079102421a66a83098
Ankit <hxankit01@gmail.com>
2025-04-10 20:41:11 +0530

    .js Cchanged

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client.js'
+import { createRoot } from 'react-dom/client';
+
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from './components/ui/sonner.jsx'
