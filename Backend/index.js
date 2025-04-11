1f589a40c93c59dc63bf457338bd249c27369009
Ankit <hxankit01@gmail.com>
2025-04-11 11:26:52 +0530

    Socket Io changes

--- Backend/index.js
+++ Backend/index.js
@@ -8,54 +8,41 @@ import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
+ 
 
 dotenv.config();
-console.log("Mongo URI:", process.env.MONGO_URI);
+ console.log(process.env.MONGO_URI);
+ 
 
 const PORT = process.env.PORT || 3000;
+
 const __dirname = path.resolve();
 
-// ======= MIDDLEWARES =======
-
-// Body parser
+//middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
-
-// CORS CONFIG
-const allowedOrigins = [
-  "http://localhost:5173",          // development
-  "https://syncrolink.onrender.com" // production
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    // allow requests with no origin (like Postman or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
+    origin: "https:/syncrolink.onrender.com",
+    credentials: true,
+    
+    optionSuccessStatus: 200
+}
 app.use(cors(corsOptions));
 
-// ======= ROUTES =======
+// yha pr apni api ayengi
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
 
-// ======= FRONTEND SERVE =======
-app.use(express.static(path.join(__dirname, "/Frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-});
 
-// ======= START SERVER =======
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+})
+
+
 server.listen(PORT, () => {
-  connectDB();
-  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
 });
--- test.js
+++ test.js
@@ -1,23 +1,10 @@
-import nodemailer from "nodemailer";
+import express from "express"
+const app=express()
+app.get("/",(req,res)=>{
+  res.send("This is ")
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: "anonymous.atma.13@gmail.com",
-    pass: "ykexkaaodllukemb", // App password
-  },
-});
-
-const mailOptions = {
-  from: "anonymous.atma.13@gmail.com",
-  to: "hxankit01@gmail.com", // test with your email
-  subject: "Test Email from Node.js",
-  text: "If you got this, the mail config is working!",
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log("Send failed:", error);
-  }
-  console.log("Email sent:", info.response);
-});
+})
+app.listen(2000,()=>{
+  console.log(("This is server"));
+  
+})
